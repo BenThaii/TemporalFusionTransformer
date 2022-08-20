177b40113ba2d29491e9c523e963f3b22b3b0e82
root <root@DESKTOP-H0QVJ1Q.localdomain>
2022-08-20 17:44:26 +0700

    first TFT docker

---
+++ TFT_docker/TFT.py
@@ -0,0 +1,218 @@
+import copy
+from pathlib import Path
+import warnings
+
+import numpy as np
+import pandas as pd
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
+from pytorch_lightning.loggers import TensorBoardLogger
+import torch
+
+from pytorch_forecasting import Baseline, TemporalFusionTransformer, TimeSeriesDataSet
+from pytorch_forecasting.data import GroupNormalizer
+from pytorch_forecasting.metrics import SMAPE, PoissonLoss, QuantileLoss
+from pytorch_forecasting.models.temporal_fusion_transformer.tuning import optimize_hyperparameters
+
+
+
+def _train(args):
+    is_distributed = len(args.hosts) > 1 and args.dist_backend is not None
+    logger.debug("Distributed training - {}".format(is_distributed))
+
+    if os.path.isdir(args.checkpoint_path):
+        print("Checkpointing directory {} exists".format(args.checkpoint_path))
+    else:
+        print("Creating Checkpointing directory {}".format(args.checkpoint_path))
+        os.mkdir(args.checkpoint_path)
+    
+    if is_distributed:
+        # Initialize the distributed environment.
+        world_size = len(args.hosts)
+        os.environ['WORLD_SIZE'] = str(world_size)
+        host_rank = args.hosts.index(args.current_host)
+        os.environ['RANK'] = str(host_rank)
+        dist.init_process_group(backend=args.dist_backend, rank=host_rank, world_size=world_size)
+        print(
+            'Initialized the distributed environment: \'{}\' backend on {} nodes. '.format(
+                args.dist_backend,
+                dist.get_world_size()) + 'Current host rank is {}. Using cuda: {}. Number of gpus: {}'.format(
+                dist.get_rank(), torch.cuda.is_available(), args.num_gpus))
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print("Device Type: {}".format(device))
+
+
+    print("Load Stallion dataset")
+    
+    
+    from pytorch_forecasting.data.examples import get_stallion_data
+
+    data = get_stallion_data()
+
+    # add time index
+    data["time_idx"] = data["date"].dt.year * 12 + data["date"].dt.month
+    data["time_idx"] -= data["time_idx"].min()
+
+    # add additional features
+    data["month"] = data.date.dt.month.astype(str).astype("category")  # categories have be strings
+    data["log_volume"] = np.log(data.volume + 1e-8)
+    data["avg_volume_by_sku"] = data.groupby(["time_idx", "sku"], observed=True).volume.transform("mean")
+    data["avg_volume_by_agency"] = data.groupby(["time_idx", "agency"], observed=True).volume.transform("mean")
+
+    # we want to encode special days as one variable and thus need to first reverse one-hot encoding
+    special_days = [
+        "easter_day",
+        "good_friday",
+        "new_year",
+        "christmas",
+        "labor_day",
+        "independence_day",
+        "revolution_day_memorial",
+        "regional_games",
+        "fifa_u_17_world_cup",
+        "football_gold_cup",
+        "beer_capital",
+        "music_fest",
+    ]
+    data[special_days] = data[special_days].apply(lambda x: x.map({0: "-", 1: x.name})).astype("category")
+
+
+    print("creating dataloader")
+    max_prediction_length = 6
+    max_encoder_length = 24
+    training_cutoff = data["time_idx"].max() - max_prediction_length
+
+    training = TimeSeriesDataSet(
+        data[lambda x: x.time_idx <= training_cutoff],
+        time_idx="time_idx",
+        target="volume",
+        group_ids=["agency", "sku"],
+        min_encoder_length=max_encoder_length // 2,  # keep encoder length long (as it is in the validation set)
+        max_encoder_length=max_encoder_length,
+        min_prediction_length=1,
+        max_prediction_length=max_prediction_length,
+        static_categoricals=["agency", "sku"],
+        static_reals=["avg_population_2017", "avg_yearly_household_income_2017"],
+        time_varying_known_categoricals=["special_days", "month"],
+        variable_groups={"special_days": special_days},  # group of categorical variables can be treated as one variable
+        time_varying_known_reals=["time_idx", "price_regular", "discount_in_percent"],
+        time_varying_unknown_categoricals=[],
+        time_varying_unknown_reals=[
+            "volume",
+            "log_volume",
+            "industry_volume",
+            "soda_volume",
+            "avg_max_temp",
+            "avg_volume_by_agency",
+            "avg_volume_by_sku",
+        ],
+        target_normalizer=GroupNormalizer(
+            groups=["agency", "sku"], transformation="softplus"
+        ),  # use softplus and normalize by group
+        add_relative_time_idx=True,
+        add_target_scales=True,
+        add_encoder_length=True,
+    )
+
+    # create validation set (predict=True) which means to predict the last max_prediction_length points in time
+    # for each series
+    validation = TimeSeriesDataSet.from_dataset(training, data, predict=True, stop_randomization=True)
+
+    # create dataloaders for model
+    batch_size = 128  # set this between 32 to 128
+    train_dataloader = training.to_dataloader(train=True, batch_size=batch_size, num_workers=0)
+    val_dataloader = validation.to_dataloader(train=False, batch_size=batch_size * 10, num_workers=0)
+
+    print("get GPU information")
+    num_GPU = 0
+    if torch.cuda.device_count() > 1:
+        num_GPU = torch.cuda.device_count()
+        print("GPU count: {}".format(num_GPU))
+
+
+    print("create model trainer")
+    # configure network and trainer
+    early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-4, patience=10, verbose=False, mode="min")
+    lr_logger = LearningRateMonitor()  # log the learning rate
+    logger = TensorBoardLogger("lightning_logs")  # logging results to a tensorboard
+
+    trainer = pl.Trainer(
+        max_epochs=30,
+        gpus=num_GPU,
+        weights_summary="top",
+        gradient_clip_val=0.1,
+        limit_train_batches=30,  # coment in for training, running valiation every 30 batches
+        # fast_dev_run=True,  # comment in to check that networkor dataset has no serious bugs
+        callbacks=[lr_logger, early_stop_callback],
+        logger=logger,
+    )
+
+    print("create model from dataset")
+    tft = TemporalFusionTransformer.from_dataset(
+        training,
+        learning_rate=0.03,
+        hidden_size=16,
+        attention_head_size=1,
+        dropout=0.1,
+        hidden_continuous_size=8,
+        output_size=7,  # 7 quantiles by default
+        loss=QuantileLoss(),
+        log_interval=10,  # uncomment for learning rate finder and otherwise, e.g. to 10 for logging every 10 batches
+        reduce_on_plateau_patience=4,
+    )
+    print(f"Number of parameters in network: {tft.size()/1e3:.1f}k")
+
+
+    print("training model")
+    trainer.fit(
+        tft,
+        train_dataloaders=train_dataloader,
+        val_dataloaders=val_dataloader,
+        ckpt_path = args.checkpoint_path
+    )
+
+    best_model_path = trainer.checkpoint_callback.best_model_path
+    best_tft = TemporalFusionTransformer.load_from_checkpoint(best_model_path)
+
+    return best_model_path
+
+
+def _save_model(model, model_dir):
+    pass
+
+
+def _save_checkpoint(model, optimizer, epoch, loss, args):
+    pass
+
+    
+def _load_checkpoint(model, optimizer, args):
+    pass
+
+    
+def model_fn(model_dir):
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--workers', type=int, default=2, metavar='W',
+                        help='number of data loading workers (default: 2)')
+    parser.add_argument('--epochs', type=int, default=2, metavar='E',
+                        help='number of total epochs to run (default: 2)')
+    parser.add_argument('--batch_size', type=int, default=4, metavar='BS',
+                        help='batch size (default: 4)')
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                        help='initial learning rate (default: 0.001)')
+    parser.add_argument('--momentum', type=float, default=0.9, metavar='M', help='momentum (default: 0.9)')
+    parser.add_argument('--dist_backend', type=str, default='gloo', help='distributed backend (default: gloo)')
+
+    parser.add_argument('--hosts', type=list, default=json.loads(os.environ['SM_HOSTS']))
+    parser.add_argument('--current-host', type=str, default=os.environ['SM_CURRENT_HOST'])
+    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAINING'])
+    parser.add_argument("--checkpoint-path",type=str,default="/opt/ml/checkpoints")
+    parser.add_argument('--num-gpus', type=int, default=os.environ['SM_NUM_GPUS'])
+
+    _train(parser.parse_args())
